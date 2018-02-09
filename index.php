c8e10c06affe8202487415c70d3721f112589d33
Bosun Egberinde <bosunski@gmail.com>
2018-02-09 15:32:42 +0100

    WEE

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 
     <title>Ticket Order History | eTicket</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/font-awesome/css/font-awesome.css" rel="stylesheet">
 
-    <link href="css/plugins/dataTables/datatables.min.css" rel="stylesheet">
+    <link href="/css/plugins/dataTables/datatables.min.css" rel="stylesheet">
 
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="/css/animate.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
 
 </head>
 
